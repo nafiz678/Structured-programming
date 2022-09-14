e38f8bded0e3a52899ba521de480effa40598035
nafiz678 <112461654+nafiz678@users.noreply.github.com>
2022-09-13 21:53:09 -0700

    Add files via upload

---
+++ Problem solving/lab task - 1/Problem 8.c
@@ -0,0 +1,20 @@
+// A program to reverse the number.
+ #include <stdio.h>
+
+int main()
+{
+    int n,num1,num2,num3,num4,num5,reverse;
+
+    printf("Enter your 5 digit number: ");
+    scanf("%d", &n);
+
+    num1=n%10;
+    num2=(n/10) %10;
+    num3=(n/100) %10;
+    num4=(n/1000) %10;
+    num5=(n/10000) %10;
+    printf("The reverse numbers are: %d%d%d%d%d" ,num1,num2, num3, num4, num5 );
+
+
+    return 0;
+}
